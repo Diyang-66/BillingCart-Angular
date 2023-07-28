fd5258f422eb284d766054709a07f5cee33ad132
diyang <dickeyyangzom6696@gmail.com>
2023-07-28 14:34:42 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app",
+    template: "<checkout></checkout>" // add any features component in this tag to check
+})
+
+export class AppComponent {
+
+
+
+ }
+
+
+
+
+//<router-outlet> </router-outlet> its a placeholder component,
+//it serves the component whichever the route provides
+
+// routing mechanism :
+
+
+//app root only job is to load the feature component
+
+//model and services are reusable data
+
+
+
+
